001bf82aa6fafe49c2c7fc8f1e8885aff0653a97
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-03-22 16:42:37 +0200

    Separate backend from frontend

---
+++ backend/src/test/java/io/github/kaurhuko/lennuplaneerija/LennuPlaneerijaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kaurhuko.lennuplaneerija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennuPlaneerijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
