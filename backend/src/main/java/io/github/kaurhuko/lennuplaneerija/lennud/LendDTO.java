75740289268d74dadf990c6ee9837928d17c7da8
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-03-22 17:20:17 +0200

    getter and setter for LendDTO

--- backend/src/main/java/io/github/kaurhuko/lennuplaneerija/lennud/LendDTO.java
+++ backend/src/main/java/io/github/kaurhuko/lennuplaneerija/lennud/LendDTO.java
@@ -20,4 +20,52 @@ public class LendDTO {
         this.saabumisAeg = lend.getSaabumisAeg();
     }
 
+    public String getLennuNumber() {
+        return lennuNumber;
+    }
+
+    public void setLennuNumber(String lennuNumber) {
+        this.lennuNumber = lennuNumber;
+    }
+
+    public String getLennuFirma() {
+        return lennuFirma;
+    }
+
+    public void setLennuFirma(String lennuFirma) {
+        this.lennuFirma = lennuFirma;
+    }
+
+    public String getValjumisKoht() {
+        return valjumisKoht;
+    }
+
+    public void setValjumisKoht(String valjumisKoht) {
+        this.valjumisKoht = valjumisKoht;
+    }
+
+    public String getSaabumisKoht() {
+        return saabumisKoht;
+    }
+
+    public void setSaabumisKoht(String saabumisKoht) {
+        this.saabumisKoht = saabumisKoht;
+    }
+
+    public LocalDateTime getValjumisAeg() {
+        return valjumisAeg;
+    }
+
+    public void setValjumisAeg(LocalDateTime valjumisAeg) {
+        this.valjumisAeg = valjumisAeg;
+    }
+
+    public LocalDateTime getSaabumisAeg() {
+        return saabumisAeg;
+    }
+
+    public void setSaabumisAeg(LocalDateTime saabumisAeg) {
+        this.saabumisAeg = saabumisAeg;
+    }
+
 }
