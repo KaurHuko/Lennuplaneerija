a2305fe39605435e959c1722177544f35a6c2c1c
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-03-22 17:03:34 +0200

    loome klassi "Lend"

---
+++ backend/src/main/java/io/github/kaurhuko/lennuplaneerija/lennud/Lend.java
@@ -0,0 +1,90 @@
+package io.github.kaurhuko.lennuplaneerija.lennud;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Lend {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String lennuNumber;
+    private String lennuFirma;
+    private String valjumisKoht;
+    private String saabumisKoht;
+    private LocalDateTime valjumisAeg;
+    private LocalDateTime saabumisAeg;
+
+    protected Lend() {}
+
+    public Lend(String lennuNumber, String lennuFirma, String valjumisKoht, String saabumisKoht, LocalDateTime valjumisAeg, LocalDateTime saabumisAeg) {
+        this.lennuNumber = lennuNumber;
+        this.lennuFirma = lennuFirma;
+        this.valjumisKoht = valjumisKoht;
+        this.saabumisKoht = saabumisKoht;
+        this.valjumisAeg = valjumisAeg;
+        this.saabumisAeg = saabumisAeg;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLennuNumber() {
+        return lennuNumber;
+    }
+
+    public void setLennuNumber(String lennuNumber) {
+        this.lennuNumber = lennuNumber;
+    }
+
+    public String getLennuFirma() {
+        return lennuFirma;
+    }
+
+    public void setLennuFirma(String lennuFirma) {
+        this.lennuFirma = lennuFirma;
+    }
+
+    public String getValjumisKoht() {
+        return valjumisKoht;
+    }
+
+    public void setValjumisKoht(String valjumisKoht) {
+        this.valjumisKoht = valjumisKoht;
+    }
+
+    public String getSaabumisKoht() {
+        return saabumisKoht;
+    }
+
+    public void setSaabumisKoht(String saabumisKoht) {
+        this.saabumisKoht = saabumisKoht;
+    }
+
+    public LocalDateTime getValjumisAeg() {
+        return valjumisAeg;
+    }
+
+    public void setValjumisAeg(LocalDateTime valjumisAeg) {
+        this.valjumisAeg = valjumisAeg;
+    }
+
+    public LocalDateTime getSaabumisAeg() {
+        return saabumisAeg;
+    }
+
+    public void setSaabumisAeg(LocalDateTime saabumisAeg) {
+        this.saabumisAeg = saabumisAeg;
+    }
+
+}
