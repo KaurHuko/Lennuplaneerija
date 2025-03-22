09d3a4bab166cb5389f4714f8f19bea0ee7e20ab
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-03-22 18:23:48 +0200

    create LendRepository

---
+++ backend/src/main/java/io/github/kaurhuko/lennuplaneerija/lennud/LendRepository.java
@@ -0,0 +1,6 @@
+package io.github.kaurhuko.lennuplaneerija.lennud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LendRepository extends JpaRepository<Lend, Long> {
+}
