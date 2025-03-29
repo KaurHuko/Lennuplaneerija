0ddd4a86108ce01dd4e453762210836daab5ace3
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-03-29 12:03:11 +0200

    style flight data into cards

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,6 +6,15 @@ const fetchInfo = ref<string>("");
 const showFetchInfo = ref<boolean>(true);
 const flightData = ref<any>([]);
 
+function formatTime(isoString: string) {
+    const date = new Date(isoString);
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+}
+
+function formatDuration(minutes: number) {
+    return Math.floor(minutes / 60) + "h " + ((minutes % 60 != 0) ? minutes % 60 + "m" : "");
+}
+
 onMounted(() => {
     fetchFlights();
 })
@@ -38,11 +47,30 @@ async function fetchFlights() {
         <div v-if="showFetchInfo">{{ fetchInfo }}</div>
 
         <div v-if="!showFetchInfo">
-            <div v-for="flight in flightData">
-                <span>{{ flight }}</span>
+            <div class="flight" v-for="flight in flightData">
+
+                <div class="departure">
+                    <span class="time">{{ formatTime(flight.valjumisAeg) }}</span>
+                    <br>
+                    <span>{{ flight.valjumisKoht }}</span>
+                </div>
+
+                <div class="duration">
+                    <p>{{ formatDuration(flight.lennuaegMinutid) }}</p>
+                </div>
+
+                <div class="arrival">
+                    <span class="time">{{ formatTime(flight.saabumisAeg) }}</span>
+                    <br>
+                    <span>{{ flight.saabumisKoht }}</span>
+                </div>
+
+                <div class="price">
+                    <p>{{ flight.hindEur }}€</p>
+                </div>
+
             </div>
         </div>
-
     </div>
 
 </template>
@@ -51,15 +79,29 @@ async function fetchFlights() {
 
 .flights-container {
     max-width: 40rem;
-    margin: 20px auto;
+    margin: 2rem auto;
     text-align: center;
 }
 
 .flight {
-    background: #f4f4f4;
-    margin: 5px 0;
-    padding: 10px;
-    border-radius: 5px;
+    background: #eeeeee;
+    margin: 1rem 0;
+    padding: 0.1rem;
+    border-radius: 2rem;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr auto;
+    align-items: center;
 }
+
+.time {
+    font-size: 2rem;
+}
+
+.price {
+    margin: 0rem 2rem;
+    font-size: 1.5rem;
+}
+
 
 </style>
