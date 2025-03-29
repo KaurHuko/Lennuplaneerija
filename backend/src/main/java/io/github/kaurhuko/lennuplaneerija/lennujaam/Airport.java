614d9fd0374c771d2c133a8596ddd536b068dae1
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-03-29 14:20:27 +0200

    remove country field to airport class

--- backend/src/main/java/io/github/kaurhuko/lennuplaneerija/lennujaam/Airport.java
+++ backend/src/main/java/io/github/kaurhuko/lennuplaneerija/lennujaam/Airport.java
@@ -3,21 +3,15 @@ package io.github.kaurhuko.lennuplaneerija.lennujaam;
 public class Airport {
 
     private final String iata;
-    private final String country;
     private final String municipality;
 
-    private Airport(String iata, String country, String municipality) {
+    Airport(String iata, String municipality) {
         this.iata = iata;
-        this.country = country;
         this.municipality = municipality;
     }
 
     public String getMunicipality() {
         return municipality;
-    }
-
-    public String getCountry() {
-        return country;
     }
 
     public String getIata() {
